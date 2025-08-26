f8bbfb3c4694ac514451a40727f7e672730c6d5e
Aryan20apr <aryansingh20nov@gmail.com>
2025-08-27 01:07:22 +0530

    Main classes

---
+++ src/main/java/com/messaging/pulsechat/PulseChatApplication.java
@@ -0,0 +1,13 @@
+package com.messaging.pulsechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseChatApplication.class, args);
+	}
+
+}
