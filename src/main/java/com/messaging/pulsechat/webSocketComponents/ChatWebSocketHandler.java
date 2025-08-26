32a9bb11cc309233579d83f935ecd7a9f0a95b74
Aryan20apr <aryansingh20nov@gmail.com>
2025-08-27 01:24:22 +0530

    Websocket configuration

---
+++ src/main/java/com/messaging/pulsechat/webSocketComponents/ChatWebSocketHandler.java
@@ -0,0 +1,7 @@
+package com.messaging.pulsechat.webSocketComponents;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class ChatWebSocketHandler extends TextWebSocketHandler{
+    
+}
